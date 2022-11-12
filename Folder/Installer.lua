4e44b4165c7f290fc6063125a30b8a5a3808d4a5
GamerPamer222 <74270354+GamerPamer222@users.noreply.github.com>
2022-11-12 16:13:13 +0100

    Update Installer.lua

--- Folder/Installer.lua
+++ Folder/Installer.lua
@@ -1,5 +1,11 @@
 local gpu = component.list("gpu")()
 local x,y = component.invoke(gpu,"getResolution")
+component.invoke(gpu,"fill",1,1,x,y," ")
+function status(text)
+    component.invoke(gpu,"fill",1,(y/2)+7,x,1," ")
+    component.invoke(gpu,"set",(x/2)-(string.len(text)/2),(y/2)+7,text)
+end
+
 while true do
-component.invoke(gpu, "fill", 1,1,x,y,"AA")
+  status("this is a broken installer")  
 end
