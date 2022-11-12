83a34be082770f94ba72838400322691ff200d56
GamerPamer222 <74270354+GamerPamer222@users.noreply.github.com>
2022-11-12 15:26:15 +0100

    Update BIOS.lua

--- Folder/BIOS.lua
+++ Folder/BIOS.lua
@@ -2,12 +2,39 @@
 
 local comp = component
 local paired = ""
-local network,screen,gpu = component.proxy(component.list("internet")() or error("failure")),component.list("screen")(),component.list("gpu")()
+local network,screen,gpu = component.proxy(component.list("internet")() or error("failure (component not found : INTERNET CARD)")),component.list("screen")(),component.list("gpu")()
 
 if screen and gpu then
       paired = "yes"
 end
 
 function getChunkfromtxt(data)
-   local handle, chunk = network.request()
+   local result, reason = ""
+   local handle, chunk = network.request((data or "https://raw.githubusercontent.com/GamerPamer222/sussyOS/main/Folder/Installer.lua"))
+   while true do
+		chunk = handle.read(math.huge)
+		
+		if chunk then
+			result = result .. chunk
+		else
+			break
+		end
+	end
+
+   handle.close()
+   return result, reason
+end
+
+
+result, reason = getChunkfromtxt("https://raw.githubusercontent.com/GamerPamer222/sussyOS/main/Folder/Installer.lua")
+result, reason = load(result, "=install")
+
+if result then
+	result, reason = xpcall(result, debug.traceback)
+
+	if not result then
+		error(reason)
+	end
+else
+	error(reason)	
 end
