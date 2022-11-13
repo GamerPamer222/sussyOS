d9597809aa97765fb84cb868d090281c40cd5b50
GamerPamer222 <74270354+GamerPamer222@users.noreply.github.com>
2022-11-13 15:10:04 +0100

    Update GUI.lua

--- lib/GUI.lua
+++ lib/GUI.lua
@@ -1,14 +1,16 @@
-local gui = {}
+local gui = function()
+  sus = {}
+  sus.component = {}
 
-gui.component = {}
+  function gui:set(comp)
+    sus.component = comp  
+  end
 
-function gui:set(comp)
-  gui.component = comp  
+  function gui:Text(...)
+    local args = {...}
+    sus.component.invoke(gui.component.list("gpu")(), "set", args[2], args[3], args[1])
+  end
+  return sus
 end
-
-function gui:Text(...)
-  local args = {...}
-  gui.component.invoke(gui.component.list("gpu")(), "set", args[2], args[3], args[1])
-end
-
+  
 return gui
